80582a420b2bc9423524ecb89704257a332c59ac
aceawan <ladeveze.quentin@openmailbox.org>
2016-11-26 21:56:58 +0100

    Add documentation for Systeme and Vehicule and Doxyfile for generating the documentation

--- systeme.h
+++ systeme.h
@@ -20,17 +20,57 @@
 #define DB_NAME "autovoitureDB"
 using namespace std;
 
+/*!
+ * \brief La classe \c Systeme est la classe qui permet d'intéragir avec les données enregistrées dans la base de données.
+ */
 class Systeme
 {
 private:
     string db_path;
 public:
+    /*!
+     * \brief Le constructeur de Systeme créé le dossier où sera stocké la base de données.
+     */
     Systeme();
+
+    /*!
+     * \brief Insère un véhicule dans la base de données
+     * \param modele modèle du dernier véhicule
+     * \param dernierControleTechnique date du dernier contrôle technique
+     * \param prix_horaire prix horaire de la location du véhicule
+     * \param prix_majoration prix horaire de la majoration si le véhicule est rendu en retard
+     * \param type_vehicule type de véhicule : voiture, vélo ou bus
+     * \details Quand le véhicule est ajouté, il est rendu disponible par défaut.
+     * \see \ref Vehicule
+     */
     void ajouterVehicule(string modele, QDate dernierControleTechnique, int prix_horaire, int prix_majoration, string type_vehicule);
+
+    /*!
+     * \brief Insère un chauffeur dans la base de données
+     * \param prenom Prénom du chauffeur
+     * \param nom Nom du chauffeur
+     * \param adresse Adresse du chauffeur
+     * \param rib RIB du chauffeur
+     */
     void ajouterChauffeur(QString prenom, QString nom, QString adresse, QString rib);
-    QSqlDatabase openDatabase();
+
+    /*!
+     * \brief Récupère l'ensemble des véhicules dans la base de données.
+     * \return Un ensemble de \ref Vehicule
+     */
     QVector<Vehicule> getVehicules();
+
+    /*!
+     * \brief Récupère l'ensemble des chauffeurs dans la base de données.
+     * \return Un ensemble de \ref Chauffeur
+     */
     QVector<Chauffeur> getChauffeurs();
+
+    /*!
+     * \brief Ouvre la base de données ou la récupère si elle est déjà ouverte
+     * \return Une \c QSqlDatabase
+     */
+    QSqlDatabase openDatabase();
 };
 
 #endif // SYSTEME_H
--- vehicule.h
+++ vehicule.h
@@ -6,26 +6,78 @@
 
 using namespace std;
 
+/*!
+ * \brief La classe véhicule représente un véhicule et les informations qui lui sont associées
+ */
 class Vehicule
 {
 private:
-    int id;
-    string modele;
-    QDate dernierControleTechnique;
-    int prix_horaire;
-    int prix_majoration;
-    bool dispo;
-    string type_vehicule;
+    int id; //! \var Identifiant unique du véhicule
+    string modele; //! \var Modèle du véhicule
+    QDate dernierControleTechnique; //! \var Date du dernier contrôle technique
+    int prix_horaire; //! \var Prix horaire de la location du véhicule
+    int prix_majoration; //! \var Prix horaire de la majoration si le véhicule est rendu en retard
+    bool dispo; //! \var Disponibilité du véhicule
+    string type_vehicule; //! \var Type de véhicule : voiture, vélo ou bus
 
 public:
+    /*!
+     * \brief Constructeur vide de \c Vehicule
+     */
     Vehicule();
+
+    /*!
+     * \brief Constructeur de \c Vehicule qui prend en paramètres toutes les informations de ce véhicule
+     * \param id Identifiant unique du véhicule
+     * \param modele Modèle du véhicule
+     * \param dernierControleTechnique Date du dernier contrôle technique
+     * \param prix_horaire Prix horaire de la location du véhicule
+     * \param prix_majoration Prix horaire de majoration si le véhicule est rendu en retard
+     * \param type_vehicule Type de véhicule
+     * \param dispo Disponibilité du véhicule
+     */
     Vehicule(int id, string modele, QDate dernierControleTechnique, int prix_horaire, int prix_majoration, string type_vehicule, bool dispo);
+
+    /*!
+     * \brief Identifiant unique du véhicule
+     * \return Un \c int
+     */
     int getId();
+
+    /*!
+     * \brief Modèle du véhicule
+     * \return Une \c string
+     */
     string getModele();
+
+    /*!
+     * \brief Date du dernier contrôle technique
+     * \return Une \c QDate
+     */
     QDate getDernierControleTechnique();
+
+    /*!
+     * \brief Prix horaire de la location du véhicule
+     * \return Un \c int
+     */
     int getPrixHoraire();
+
+    /*!
+     * \brief Prix horaire de la majoration si le véhicule est rendu en retard
+     * \return Un \c int
+     */
     int getPrixMajoration();
+
+    /*!
+     * \brief Type de véhicule
+     * \return un \c string
+     */
     string getTypeVehicule();
+
+    /*!
+     * \brief Disponibilité du véhicule
+     * \return Un booléen à vrai si le véhicule est en service, à faux sinon.
+     */
     bool isDispo();
 };
 
